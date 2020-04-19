c8926394df8f30f5d4265ad120f64a1c6784ceb9
noahchristensen <nchri003@ucr.edu>
2020-04-19 16:24:35 -0700

    Add Search functionality
    completed basic search gui, found through search most used

--- Client/HelloWindowsDesktop.cpp
+++ Client/HelloWindowsDesktop.cpp
@@ -32,8 +32,22 @@ HINSTANCE hInst;
 #define ID_BUTTON_7 7 // usage comparison
 #define ID_BUTTON_8 8 // busiest location
 
+// Search
+#define ID_SEARCH_TIME 9 // busiest location
+#define ID_SEARCH_DAY 10 // enter time
+#define ID_SEARCH_BUTTON 11 // enter day
+#define ID_SEARCH_DATA 12 // enter day
+
 // Forward declarations of functions included in this code module:
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void RegisterSearch(HINSTANCE);
+
+LRESULT CALLBACK SearchProcedure(HWND, UINT, WPARAM, LPARAM);
+
+void DisplaySearch(HWND);
+
+void SendRequest();
 
 int CALLBACK WinMain(
     _In_ HINSTANCE hInstance,
@@ -66,6 +80,8 @@ int CALLBACK WinMain(
 
         return 1;
     }
+
+    RegisterSearch(hInstance);
 
     // Store instance handle in our global variable
     hInst = hInstance;
@@ -109,7 +125,7 @@ int CALLBACK WinMain(
     HWND hwndButton1 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Modify the Data",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         250,         // x position 
         300,         // y position 
         180,        // Button width
@@ -123,7 +139,7 @@ int CALLBACK WinMain(
     HWND hwndButton2 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Search Most Used",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         650,         // x position 
         300,         // y position 
         180,        // Button width
@@ -137,7 +153,7 @@ int CALLBACK WinMain(
     HWND hwndButton3 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Search Least Used",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         1050,         // x position 
         300,         // y position 
         180,        // Button width
@@ -151,7 +167,7 @@ int CALLBACK WinMain(
     HWND hwndButton4 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Time Between Pickups",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         1450,         // x position 
         300,         // y position 
         180,        // Button width
@@ -165,7 +181,7 @@ int CALLBACK WinMain(
     HWND hwndButton5 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Search Active Vehicles",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         250,         // x position 
         600,         // y position 
         180,        // Button width
@@ -179,7 +195,7 @@ int CALLBACK WinMain(
     HWND hwndButton6 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Ratio for Pickups",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         650,         // x position 
         600,         // y position 
         180,        // Button width
@@ -193,7 +209,7 @@ int CALLBACK WinMain(
     HWND hwndButton7 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Two App Comparison",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         1050,         // x position 
         600,         // y position 
         180,        // Button width
@@ -207,7 +223,7 @@ int CALLBACK WinMain(
     HWND hwndButton8 = CreateWindow(
         L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
         L"Busiest Location",      // Button text 
-        WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON | BS_NOTIFY,  // Styles 
+        WS_VISIBLE | WS_CHILD,  // Styles 
         1450,         // x position 
         600,         // y position 
         180,        // Button width
@@ -246,8 +262,6 @@ int CALLBACK WinMain(
     return (int)msg.wParam;
 }
 
-void SendRequest();
-
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
 //  PURPOSE:  Processes messages for the main window.
@@ -262,87 +276,233 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
     switch (message)
     {
-    case WM_PAINT:
-        hdc = BeginPaint(hWnd, &ps);
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
 
-        // Here application is laid out.
-        TextOut(hdc,
-            5, 5,
-            greeting, _tcslen(greeting));
-        // End application-specific layout section.
+            // Here application is laid out.
+            TextOut(hdc,
+                5, 5,
+                greeting, _tcslen(greeting));
+            // End application-specific layout section.
 
-        EndPaint(hWnd, &ps);
-        break;
-    case WM_COMMAND: // when an action happens
-            switch(LOWORD(wParam))
-            {
-                case ID_BUTTON_1:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Modify the Data was clicked"), TEXT("CS180 Project"), MB_OK);
+            EndPaint(hWnd, &ps);
+            break;
+        case WM_CREATE:
+            break;
+        case WM_COMMAND: // when an action happens
+                switch(LOWORD(wParam))
+                {
+                    case ID_BUTTON_1:
+                        ::MessageBeep(MB_ICONERROR);
+                        ::MessageBox(hWnd, TEXT("Modify the Data not yet implemented"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        //SendRequest();
 
-                    break;
-                case ID_BUTTON_2:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Search Most Used"), TEXT("CS180 Project"), MB_OK);
+                        break;
+                    case ID_BUTTON_2:
+                        //::MessageBox(hWnd, TEXT("Search Most Used was clicked"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        DisplaySearch(hWnd);
+                        //SendRequest();
 
-                    break;
-                case ID_BUTTON_3:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Search Least Used was clicked"), TEXT("CS180 Project"), MB_OK);
+                        break;
+                    case ID_BUTTON_3:
+                        ::MessageBeep(MB_ICONERROR);
+                        ::MessageBox(hWnd, TEXT("Search Least Used not yet implemented"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        //SendRequest();
 
-                    break;
-                case ID_BUTTON_4:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Time Between Pickups was clicked"), TEXT("CS180 Project"), MB_OK);
+                        break;
+                    case ID_BUTTON_4:
+                        ::MessageBeep(MB_ICONERROR);
+                        ::MessageBox(hWnd, TEXT("Time Between Pickups not yet implemented"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        //SendRequest();
 
-                    break;
-                case ID_BUTTON_5:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Search Active Vehicles was clicked"), TEXT("CS180 Project"), MB_OK);
+                        break;
+                    case ID_BUTTON_5:
+                        ::MessageBeep(MB_ICONERROR);
+                        ::MessageBox(hWnd, TEXT("Search Active Vehicles not yet implemented"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        //SendRequest();
 
-                    break;
-                case ID_BUTTON_6:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Ratio for Pickups was clicked"), TEXT("CS180 Project"), MB_OK);
+                        break;
+                    case ID_BUTTON_6:
+                        ::MessageBeep(MB_ICONERROR);
+                        ::MessageBox(hWnd, TEXT("Ratio for Pickups not yet implemented"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        //SendRequest();
 
-                    break;
-                case ID_BUTTON_7:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Two App Comparison was clicked"), TEXT("CS180 Project"), MB_OK);
+                        break;
+                    case ID_BUTTON_7:
+                        ::MessageBeep(MB_ICONERROR);
+                        ::MessageBox(hWnd, TEXT("Two App Comparison not yet implemented"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        //SendRequest();
 
-                    break;
-                case ID_BUTTON_8:
-                    ::MessageBeep(MB_ICONERROR);
-                    ::MessageBox(hWnd, TEXT("Busiest Location was clicked"), TEXT("CS180 Project"), MB_OK);
+                        break;
+                    case ID_BUTTON_8:
+                        ::MessageBeep(MB_ICONERROR);
+                        ::MessageBox(hWnd, TEXT("Busiest Location not yet implemented"), TEXT("CS180 Project"), MB_OK);
 
-                    //SendRequest();
+                        //SendRequest();
 
-                    break;
-            }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-        break;
+                        break;
+                }
+                break;
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+            break;
     }
 
     return 0;
+}
+
+void RegisterSearch(HINSTANCE hInstance)
+{
+    WNDCLASSW search = { 0 };
+
+    search.hbrBackground = (HBRUSH)(COLOR_WINDOW);
+    search.hCursor = LoadCursor(NULL, IDC_ARROW);
+    search.style = CS_HREDRAW | CS_VREDRAW;
+    search.hInstance = hInstance;
+    search.lpszClassName = L"mySearchClass";
+    search.lpfnWndProc = SearchProcedure;
+
+    RegisterClassW(&search);
+}
+
+HWND hwndTimeField;
+HWND hwndDayField;
+
+LRESULT CALLBACK SearchProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lp)
+{
+    switch (msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hWnd);
+            break;
+        case WM_COMMAND: // when an action happens
+            switch (LOWORD(wParam))
+            {
+            case ID_SEARCH_BUTTON:
+                wchar_t timeText[10];
+                GetWindowText(hwndTimeField, timeText, 10);
+                wchar_t dayText[10];
+                GetWindowText(hwndDayField, dayText, 10);
+                ::MessageBox(hWnd, timeText, TEXT("CS180 Project - Time"), MB_OK);
+                ::MessageBox(hWnd, dayText, TEXT("CS180 Project - Day"), MB_OK);
+
+                //SendRequest();
+
+                break;
+            }
+            break;
+        default:
+            return DefWindowProcW(hWnd, msg, wParam, lp);
+    }
+}
+
+void DisplaySearch(HWND hWnd)
+{
+    HWND hWndSearch = CreateWindowW(
+        L"mySearchClass",
+        L"CS180 Project - Search",
+        WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+        400, 400, 200, 200,
+        hWnd,
+        NULL,
+        NULL,
+        NULL
+    );
+
+    HWND hwndTimeLabel = CreateWindow(
+        L"Static",  // Predefined class; Unicode assumed //STATIC, Edit
+        L"Time of Day (\"XX:XX\")",      // Button text 
+        WS_VISIBLE | WS_CHILD | BS_CENTER,  // Styles 
+        50,         // x position 
+        50,         // y position 
+        400,        // Button width
+        50,        // Button heighth
+        hWndSearch,     // Parent window
+        NULL,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );      // Pointer not needed.
+
+    hwndTimeField = CreateWindow(
+        L"EDIT",  // Predefined class; Unicode assumed //STATIC, Edit
+        L"",      // Button text 
+        WS_VISIBLE | WS_CHILD | WS_BORDER,  // Styles 
+        50,         // x position 
+        100,         // y position 
+        400,        // Button width
+        50,        // Button heighth
+        hWndSearch,     // Parent window
+        (HMENU)ID_SEARCH_TIME,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );      // Pointer not needed.
+
+    HWND hwndDayLabel = CreateWindow(
+        L"Static",  // Predefined class; Unicode assumed //STATIC, Edit
+        L"Day",      // Button text 
+        WS_VISIBLE | WS_CHILD | BS_CENTER,  // Styles 
+        600,         // x position 
+        50,         // y position 
+        400,        // Button width
+        50,        // Button heighth
+        hWndSearch,     // Parent window
+        NULL,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );      // Pointer not needed.
+
+    hwndDayField = CreateWindow(
+        L"EDIT",  // Predefined class; Unicode assumed //STATIC, Edit
+        L"",      // Button text 
+        WS_VISIBLE | WS_CHILD | WS_BORDER,  // Styles 
+        600,         // x position 
+        100,         // y position 
+        400,        // Button width
+        50,        // Button heighth
+        hWndSearch,     // Parent window
+        (HMENU)ID_SEARCH_DAY,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );      // Pointer not needed.
+
+    HWND hwndSearchButton = CreateWindow(
+        L"BUTTON",  // Predefined class; Unicode assumed //STATIC, Edit
+        L"Search",      // Button text 
+        WS_VISIBLE | WS_CHILD,  // Styles 
+        1650,         // x position 
+        50,         // y position 
+        100,        // Button width
+        100,        // Button heighth
+        hWndSearch,     // Parent window
+        (HMENU)ID_SEARCH_BUTTON,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );      // Pointer not needed.
+
+    HWND hwndSearchData = CreateWindow(
+        L"STATIC",  // Predefined class; Unicode assumed //STATIC, Edit
+        L"Data goes here",      // Button text 
+        WS_VISIBLE | WS_CHILD | WS_BORDER,  // Styles 
+        50,         // x position 
+        200,         // y position 
+        1500,        // Button width
+        800,        // Button heighth
+        hWndSearch,     // Parent window
+        (HMENU)ID_SEARCH_DATA,       // No menu.
+        (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+        NULL
+    );      // Pointer not needed.
 }
 
 void SendRequest() // send request to server
@@ -394,3 +554,4 @@ void SendRequest() // send request to server
     // Close down Winsock
     WSACleanup();
 }
+
