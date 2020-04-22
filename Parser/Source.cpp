9cd19401bf5d339f0dc8498a0847fb647bbb8bff
Jenaro <jvega008@ucr.edu>
2020-04-22 01:57:01 -0700

    search functions - server needs to send results to client

--- Parser/Source.cpp
+++ Parser/Source.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include "search.h"
 using namespace std;
 
 void parseLine(string line, vector<string>& results, int index) {
@@ -11,7 +12,7 @@ void parseLine(string line, vector<string>& results, int index) {
     vector<string> results1;
 
     while (getline(X, column, ',')) { //splits line into columns 
-         results1.push_back(column);
+        results1.push_back(column);
     }
 
     results = results1;
@@ -27,139 +28,146 @@ void read(vector<vector<string>>& result)
     // Open an existing file
     fin.open("uber-raw-data-apr14.csv");
     int index = 0;
-    vector<string> results; 
+    vector<string> results;
 
     while (!fin.eof()) {
         fin >> line1;   //These two are to format the first column of the csv file
-        fin >> line2;   
+        fin >> line2;
         line1 = line1 + " " + line2; // combines the date column string with the rest of its corresponding row "4/1/2014 + 0:11:00",40.769,-73.9549,"B02512"
         parseLine(line1, results, index); // parses line1 to results vector with four elements:  "Date Time", Lat, Lon, "Base",
         result.push_back(results); //pushes results vector to result vector
-        if (index % 10000 == 0) {
-            cout << "Parsed " << index << " lines" <<  endl;
-        }
         index++;
     }
     return;
-}
+};
 
-
-
-int main()
-{
+void searchDate(vector<vector<string>>& results, vector<vector<string>>& csvData, vector<string>& searchInputs) {
     vector<string> miniVec;
-    vector<vector<string>> results;
-    cout << "Wait..." << endl;
-
-    read(results); //reads csv file and parses data into vector of vector<string>
-    string input1;
-    string input2;
-    string input3;
-    string input4;
-    string input5;
-    string searchOption;
-    vector < vector<string>> searchRes;
-    while (searchOption != "q") {
-        searchRes.clear();
-        cout << "Choose search option: " << endl;
-        cout << "a: Time/Date" << endl;
-        cout << "b: Location" << endl;
-        cout << "c: Base#" << endl;
-        cout << "d: Time/Date/Location/Base#" << endl;
-        cout << "q: Quit" << endl;
-
-            getline(cin, searchOption);
-
-        if (searchOption == "q") {
-            cout << "Bye..." << endl;
-            return 0;
-        }
-        if (searchOption == "a") {
-            cout << "Enter date XX/XX/XXXX : " << endl;
-            getline(cin, input1);
-
-            cout << "Enter time XX:XX : " << endl;
-            getline(cin, input2);
-
-            input1 = "\"" + input1 + " " + input2 + ":00" + "\""; // formats input to "XX/XX/XXXX 00:00:00"
-            cout << "Entered: " << input1 << endl;
-
-            for (unsigned int k = 0; k < results.size(); k++) {         // searches for time and day "XX/XX/XXXX 00:00:00" 
-                miniVec = results.at(k);
-                if (input1.compare(miniVec.at(0)) == 0) {
-                    searchRes.push_back(miniVec);
-                }
-            }
-        }
-        else if (searchOption == "b") {
-            cout << "Enter Longitude: " << endl;
-            getline(cin, input1);
-
-            cout << "Enter Latitude: " << endl;
-            getline(cin, input2);
-
-            cout << "Entered: " << "Lon: " << input1 << " " << "Lat: " << input2 << endl;
-
-            for (unsigned int k = 0; k < results.size(); k++) {         // searches for Location: lon/lat 
-                miniVec = results.at(k);
-                if (input1.compare(miniVec.at(1)) == 0 && input2.compare(miniVec.at(2)) == 0) {
-                    searchRes.push_back(miniVec);
-                }
-            }
-
-        }
-        else if (searchOption == "c") {
-            cout << "Enter Base# BXXXXX: " << endl;
-            getline(cin, input1);
-
-            cout << "Entered: " << "Base#: " << input1 << endl;
-            input1 = "\"" + input1 + "\""; //formats input to "BXXXXX"
-
-            for (unsigned int k = 0; k < results.size(); k++) {         // searches for Base#: BXXXXX
-                miniVec = results.at(k);
-                if (input1.compare(miniVec.at(3)) == 0) {
-                    searchRes.push_back(miniVec);
-                }
-            }
-        }
-        else if (searchOption == "d") {
-            cout << "Enter date XX/XX/XXXX : " << endl;
-            getline(cin, input1);
-
-            cout << "Enter time XX:XX : " << endl;
-            getline(cin, input2);
-
-            cout << "Enter Longitude: " << endl;
-            getline(cin, input3);
-
-            cout << "Enter Latitude: " << endl;
-            getline(cin, input4);
-
-            cout << "Enter Base# BXXXXX: " << endl;
-            getline(cin, input5);
-
-            input1 = "\"" + input1 + " " + input2 + ":00" + "\""; // formats input to "XX/XX/XXXX 00:00:00"
-            input5 = "\"" + input5 + "\""; //formats input to "BXXXXX"
-
-            for (unsigned int k = 0; k < results.size(); k++) {         // searches for Location: lon/lat 
-                miniVec = results.at(k);
-                if (input1.compare(miniVec.at(0)) == 0 && input3.compare(miniVec.at(1)) == 0 && input4.compare(miniVec.at(2)) == 0 && input5.compare(miniVec.at(3)) == 0) {
-                   
-                    searchRes.push_back(miniVec);
-                }
-            }
-
-        }
-
-        // prints results stored in searchRes vector
-        cout << "Results: " << endl;
-        for (unsigned int j = 0; j < searchRes.size(); j++) {
-            miniVec = searchRes.at(j);
-            for (unsigned int l = 0; l < miniVec.size(); l++) {
-                cout << miniVec.at(l) << " ";
-            }
-            cout << endl;
+    for (unsigned int k = 0; k < csvData.size(); k++) {         // searches for date XX/XX/XXXX 
+        miniVec = csvData.at(k);
+        if (miniVec.at(0).find(searchInputs.at(0)) != string::npos) {
+            results.push_back(miniVec);
         }
     }
-    return 0;
-}
+};
+
+void searchTime(vector<vector<string>>& results, vector<vector<string>>& csvData, vector<string>& searchInputs) {
+    vector<string> miniVec;
+    searchInputs.at(0) = searchInputs.at(0) + ":00";
+    for (unsigned int k = 0; k < csvData.size(); k++) {         // searches for time 00:00
+        miniVec = csvData.at(k);
+        if (miniVec.at(0).find(searchInputs.at(0)) != string::npos) {
+            results.push_back(miniVec);
+        }
+    }
+};
+
+void searchDateTime(vector<vector<string>>& results, vector<vector<string>>& csvData, vector<string>& searchInputs) {
+    vector<string> miniVec;
+    string dateAndTime = "\"" + searchInputs.at(1) + " " + searchInputs.at(0)  + ":00" + "\"";
+    for (unsigned int k = 0; k < csvData.size(); k++) {         // searches for time and day "XX/XX/XXXX 00:00:00" 
+        miniVec = csvData.at(k);
+        if (dateAndTime.compare(miniVec.at(0)) == 0) {
+            results.push_back(miniVec);
+        }
+    }
+};
+
+void searchLocation(vector<vector<string>>& results, vector<vector<string>>& csvData, vector<string>& searchInputs) {
+    vector<string> miniVec;
+
+    for (unsigned int k = 0; k < csvData.size(); k++) {         // searches for Location: lon/lat 
+        miniVec = csvData.at(k);
+        if (searchInputs.at(0).compare(miniVec.at(1)) == 0 && searchInputs.at(1).compare(miniVec.at(2)) == 0) {
+            results.push_back(miniVec);
+        }
+    }
+};
+
+void searchBase(vector<vector<string>>& results, vector<vector<string>>& csvData, vector<string>& searchInputs) {
+    vector<string> miniVec;
+    searchInputs.at(0) = "\"" + searchInputs.at(0) + "\"";
+    for (unsigned int k = 0; k < csvData.size(); k++) {         // searches for Base#: BXXXXX
+        miniVec = csvData.at(k);
+        if (searchInputs.at(0).compare(miniVec.at(3)) == 0) {
+            results.push_back(miniVec);
+        }
+    }
+};
+
+void searchSpecific(vector<vector<string>>& results, vector<vector<string>>& csvData, vector<string>& searchInputs) {
+    vector<string> miniVec;
+    searchInputs.at(4) = "\"" + searchInputs.at(4) + "\"";
+    string dateAndTime = "\"" + searchInputs.at(1) + " " + searchInputs.at(0) + ":00" + "\"";
+    for (unsigned int k = 0; k < csvData.size(); k++) {         // searches for Location: lon/lat 
+        miniVec = csvData.at(k);
+        if (dateAndTime.compare(miniVec.at(0)) == 0 && searchInputs.at(2).compare(miniVec.at(1)) == 0 && searchInputs.at(3).compare(miniVec.at(2)) == 0 && searchInputs.at(4).compare(miniVec.at(3)) == 0) {
+            results.push_back(miniVec);
+        }
+    }
+};
+
+void parseClient(char buf[1024], vector<vector<string>>& csvData, vector<vector<string>>& results) {
+    int timeFlag = 0;
+    int dateFlag = 0;
+    int locationFlag = 0;
+    int baseFlag = 0;
+    int mostFlag = 0;
+    int leastFlag = 0;
+    vector<string> clientDat;
+    stringstream X(buf);
+    string columns;
+    string column;
+    vector<string> searchInputs;
+    while (getline(X, columns, ',')) {
+        clientDat.push_back(columns);
+    }
+    for (unsigned i = 0; i < clientDat.size(); i++) {
+
+        string compare = "Time: ";
+        if (clientDat.at(i).find(compare) != string::npos) {
+
+            if (compare != clientDat.at(i)) {
+
+                string::size_type f = clientDat.at(i).find(compare);
+                clientDat.at(i).erase(f, compare.length());
+                searchInputs.push_back(clientDat.at(i));
+                timeFlag = 1;
+            }
+        }
+        compare = "Date: ";
+        if (compare.find(clientDat.at(i)) != string::npos) {
+            if (compare != clientDat.at(i)) {
+
+                string::size_type f = clientDat.at(i).find(compare);
+                clientDat.at(i).erase(f, compare.length());
+                searchInputs.push_back(clientDat.at(i));
+                dateFlag = 1;
+            }
+        }
+
+        for (unsigned h = 0; h < searchInputs.size(); h++) {
+            cout << searchInputs.at(h) << " " << endl;
+        }
+
+        if (timeFlag && dateFlag && locationFlag && baseFlag) {
+            searchSpecific(results, csvData, searchInputs);
+        }
+        else if (timeFlag && dateFlag) {
+            searchDateTime(results, csvData, searchInputs);
+        }
+        else if (timeFlag) {
+            searchTime(results, csvData, searchInputs);
+        }
+        else if (dateFlag) {
+            searchDate(results, csvData, searchInputs);
+        }
+        else if (locationFlag) {
+            searchLocation(results, csvData, searchInputs);
+        }
+        else if (baseFlag) {
+            searchBase(results, csvData, searchInputs);
+        }
+    }
+    return;
+};
